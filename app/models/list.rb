3f0ba0c1c308020a0badc5c2cad2244bdd94772b
Sergio Sosa Romero <smatias.sosa@gmail.com>
2022-06-02 23:09:01 -0300

    gema agregada

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy  #en caso de borrar lista va a destruir todos los bookmarks que tenga esta lista
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
-
+  has_one_attached :photo
 end
